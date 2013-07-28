84c485a4da38bb76cc558f4b57b2382208d64c8b
Andreas Frischknecht <andreas.frischknecht@gmail.com>
2013-07-28 13:25:54 +0200

    Added Fake build script.

---
+++ VS11/Fsharp.By.Example/Example.Basics.fs
@@ -0,0 +1,13 @@
+﻿module Example.Basics
+
+//----------------------------
+// 1. Basic language usage
+//----------------------------
+
+// 1.1 Integer arithmetic
+let IntegerArithmetic() =
+    let x = 15 + 3
+    let y = 15 - 3
+    printfn "x = %d, y = %d" x y
+
+
